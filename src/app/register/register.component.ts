8c8a3f2039a775d4602627fef3bd278a5572b702
DESKTOP-4V6GCAP\Tungpc <tungbk09c1a@gmail.com>
2023-03-23 19:41:26 +0700

    tung up

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -13,8 +13,16 @@ export class RegisterComponent implements OnInit {
   public errorMessage = '';
   public formRegister :FormGroup | any
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder) { 
+    
+  }
+  bankName = '';
+  selectedBank: number | undefined;
+  bankNameLists = [{id: 1, name: "VIETCOMBANK"}];
 
+onBankChange(event: any) {
+  console.log(event);
+}
   ngOnInit(): void {
     this.formRegister = this.fb.group({
       fullName: ['', Validators.required],
@@ -22,6 +30,10 @@ export class RegisterComponent implements OnInit {
       email: ['', [Validators.required, Validators.email]],
 
     });
+    // this.bankNameLists =[ {id: 1, name: "VIETCOMBANK"}]
+    this.bankNameLists = [{id: 1, name: "VIETCOMBANK"}];
+    console.log(this.bankNameLists)
+    
   }
 
   public onSubmit(): void {
