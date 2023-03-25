000a0c614b2f2782b58a77a0f4e40d4cbdcda532
DESKTOP-4V6GCAP\Tungpc <tungbk09c1a@gmail.com>
2023-03-25 21:07:34 +0700

    xxxxxx

--- src/app/account-info/account-info.component.ts
+++ src/app/account-info/account-info.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup } from '@angular/forms';
+import { Account } from './account';
 
 @Component({
   selector: 'app-account-info',
   templateUrl: './account-info.component.html',
   styleUrls: ['./account-info.component.scss']
 })
-export class AccountInfoComponent {
+
+  export class AccountInfoComponent implements OnInit {
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  public formChangePass :FormGroup | any
+  id: any;
+  nameAccount: any;
+  nameBank : any;
+  numberBank: any;
+  
   gameLists:any;
   selectGame: any;
+
+  
 isborderli = [false, false, false,false,false,false];
 selectedTab: number = 0;
 
@@ -18,4 +32,10 @@ activeInfo(index: number) {
      
     }
   }
+
+  accounts: Account[] = [
+    { id: 1, nameAccount: 'John', nameBank: 'xx' ,numberBank: 123231231},
+    { id: 2, nameAccount: 'John', nameBank: 'xx' ,numberBank: 123231231},
+    { id: 3, nameAccount: 'John', nameBank: 'xx' ,numberBank: 123231231},
+  ];
 }
---
+++ src/app/account-info/account.ts
@@ -0,0 +1,6 @@
+export class Account {
+    id: number | undefined;
+    nameAccount: string | undefined;
+    nameBank: string | undefined;
+    numberBank: number | undefined;
+  }
