515f20baf6143e63f8c6c707d3966a8998aca981
tri72027 <tri72027@gmail.com>
2023-03-31 21:43:07 +0700

    no message

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-
+import { NgbCarouselConfig, NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { CommonModule, NgIf } from '@angular/common';
 @Component({
   selector: 'app-carousel',
   template: `
@@ -12,13 +13,22 @@ import { Component } from '@angular/core';
     </div>
   </ng-template>
 </ngb-carousel>`,
-  styleUrls: ['./carousel.component.scss']
+  styleUrls: ['./carousel.component.scss'],
+  providers: [NgbCarouselConfig],
 })
 export class CarouselComponent {
-  images = ['assets/images/slide1.png', 'assets/images/slide1.png', 'assets/images/slide1.png'];
+  images = ['assets/images/slide1.png', 'assets/images/slide2.png', 'assets/images/slide3.png', 'assets/images/slide4.png'];
   showNavigationArrows = true;
   showNavigationIndicators = true;
+  interval = 50000
   ngOnInit(): void {
 
+  }
+  constructor(config: NgbCarouselConfig) {
+    // customize default values of carousels used by this component tree
+    config.interval = 5000;
+    config.wrap = false;
+    config.keyboard = false;
+    config.pauseOnHover = false;
   }
 }
